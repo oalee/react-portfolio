b8057eba1c586314c3173bacdd1ccc44349d709c
Ali Rahimi <ilaimihar@gmail.com>
2020-11-24 20:57:55 +0330

    updated works

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -2,44 +2,41 @@ import React, { Component } from 'react';
 
 import Slider from "react-slick";
 
-import CarouselImageList from './CarouselImageList'
-var settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1
-};
+
+
+var baseImgName = 'signal-2020-11-24-202038_001.jpeg'
+
+var imageList = new Array()
+
+for (var i =1; i<10; i++){
+  imageList.push(`images/screenshots/signal-2020-11-24-202038_00${i}.jpeg`)
+}
+
+for (var i =10; i<13; i++){
+  imageList.push(`images/screenshots/signal-2020-11-24-202038_0${i}.jpeg`)
+}
+
+console.log(imageList)
+var images = imageList.map( function(item) {
+
+  return <div> 
+
+    <img src={item} />
+  </div>
+  
+} )
+
 class Portfolio extends Component {
   render() {
-
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
-          </div>
-        </div>
-      })
-    }
 
     var settings = {
       dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 2,
+      slidesToShow: 3,
+      slidesToScroll: 1,
       autoplay: true
+    
     };
 
     return (
@@ -54,30 +51,24 @@ class Portfolio extends Component {
         <div className="screenshotsContainer">
         <style jsx>{`
         .screenshotsContainer {
-          margin: 50px;
+     
         }
         li {
           display: inline-block;
         }
-        img{
-          margin: auto;
+        .slick-slide img{
+          border-radius: 8px;
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          width: 80%;
 
         }
       `}</style>
         <Slider {...settings}>
-          <div>
-            <img src="images/screenshots/screenshot1.jpeg"
-           />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
+      {
+       images
+      }
         </Slider>
 
           </div>
